1de95236a2cd6f07354b47b71613606f8c23af67
Nishita Ervantikar <nishita.ervantikar@spectraforce.com>
2025-05-28 20:55:55 +0530

    test

--- logout.js
+++ logout.js
@@ -2,5 +2,5 @@
 const performAttendance = require('./login'); // reusing the same function
 
 if (require.main === module) {
-  performAttendance('#LogoutButtonId', 'Logout');  // replace '#LogoutButtonId' with actual ID for logout button
+  performAttendance('#divEndDay', 'Logout');  // replace '#LogoutButtonId' with actual ID for logout button
 }
